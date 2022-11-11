2755947ad3f68264f059f6d3ac015d8f67cd2c5b
aivilouis <olivia.yusita@student.umn.ac.id>
2022-11-11 21:23:17 +0700

    bottom navbar
    - icon home blm rata

--- app/src/main/java/umn/ac/id/level/Home.java
+++ app/src/main/java/umn/ac/id/level/Home.java
@@ -3,16 +3,46 @@ package umn.ac.id.level;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class Home extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.activity_home);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.home_actionbar);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()) {
+                case R.id.action_home:
+                    return true;
+                case R.id.action_addpost:
+                    startActivity(new Intent(getApplicationContext(), AddPost.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.action_save:
+                    startActivity(new Intent(getApplicationContext(), Saved.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.action_account:
+                    startActivity(new Intent(getApplicationContext(), Account.class));
+                    overridePendingTransition(0,0);
+                    return true;
+            }
+            return false;
+        });
     }
 }
