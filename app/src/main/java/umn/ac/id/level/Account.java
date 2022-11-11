5ca66bb46fe61151971026702f0f57f62e087c34
aivilouis <olivia.yusita@student.umn.ac.id>
2022-11-11 22:08:12 +0700

    create all bottom navbar & actionbar

--- app/src/main/java/umn/ac/id/level/Account.java
+++ app/src/main/java/umn/ac/id/level/Account.java
@@ -2,13 +2,45 @@ package umn.ac.id.level;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class Account extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.action_account);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()) {
+                case R.id.action_home:
+                    startActivity(new Intent(getApplicationContext(), Home.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.action_addpost:
+                    startActivity(new Intent(getApplicationContext(), AddPost.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.action_save:
+                    startActivity(new Intent(getApplicationContext(), Saved.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.action_account:
+                    return true;
+            }
+            return false;
+        });
     }
 }
--- app/src/main/java/umn/ac/id/level/Saved.java
+++ app/src/main/java/umn/ac/id/level/Saved.java
@@ -1,14 +1,48 @@
 package umn.ac.id.level;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class Saved extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.saved_actionbar);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.action_save);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()) {
+                case R.id.action_home:
+                    startActivity(new Intent(getApplicationContext(), Home.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.action_addpost:
+                    startActivity(new Intent(getApplicationContext(), AddPost.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.action_save:
+                    return true;
+                case R.id.action_account:
+                    startActivity(new Intent(getApplicationContext(), Account.class));
+                    overridePendingTransition(0,0);
+                    return true;
+            }
+            return false;
+        });
     }
 }
