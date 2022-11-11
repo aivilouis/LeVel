1f5c850289b0bcf553355565b612d04971257e4a
aivilouis <olivia.yusita@student.umn.ac.id>
2022-11-11 15:49:25 +0700

    add post details actionbar

--- app/src/main/java/umn/ac/id/level/AddPostDetails.java
+++ app/src/main/java/umn/ac/id/level/AddPostDetails.java
@@ -1,8 +1,14 @@
 package umn.ac.id.level;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class AddPostDetails extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class AddPostDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post_details);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.addpost_actionbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.cancel_icon);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_addpost, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_next) {
+            Intent intent = new Intent(AddPostDetails.this, Home.class);
+            this.startActivity(intent);
+            return true;
+        }
+        if (id == android.R.id.home) {
+            Intent intent = new Intent(AddPostDetails.this, AddPost.class);
+            this.startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
