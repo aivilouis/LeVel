45983965efd530d1afb8043d493b0ef24a6d9c57
brigittx <80472623+brigittx@users.noreply.github.com>
2022-12-03 15:59:19 +0700

    faq list

---
+++ app/src/main/java/umn/ac/id/level/ExpandListChild.java
@@ -0,0 +1,22 @@
+package umn.ac.id.level;
+
+public class ExpandListChild {
+    private String Name;
+    private String Tag;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getTag() {
+        return Tag;
+    }
+
+    public void setTag(String Tag) {
+        this.Tag = Tag;
+    }
+}
--- app/src/main/java/umn/ac/id/level/FAQ.java
+++ app/src/main/java/umn/ac/id/level/FAQ.java
@@ -8,20 +8,87 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class FAQ extends AppCompatActivity {
     private Bitmap bm;
+    private ExpandListAdapter ExpAdapter;
+    private ArrayList<ExpandListGroup> ExpListItems;
+    private ExpandableListView ExpandList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        ExpandList = (ExpandableListView) findViewById(R.id.ExpList);
+        ExpListItems = SetStandardGroups();
+        ExpAdapter = new ExpandListAdapter(FAQ.this, ExpListItems);
+        ExpandList.setAdapter(ExpAdapter);
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.faq_actionbar);
 
+    }
+
+    public ArrayList<ExpandListGroup> SetStandardGroups() {
+        ArrayList<ExpandListGroup> list = new ArrayList<ExpandListGroup>();
+        ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
+        ExpandListGroup gru1 = new ExpandListGroup();
+        gru1.setName("File");
+        ExpandListChild ch1_1 = new ExpandListChild();
+        ch1_1.setName("New");
+        ch1_1.setTag(null);
+        list2.add(ch1_1);
+        ExpandListChild ch1_2 = new ExpandListChild();
+        ch1_2.setName("Open");
+        ch1_2.setTag(null);
+        list2.add(ch1_2);
+        ExpandListChild ch1_3 = new ExpandListChild();
+        ch1_3.setName("Close");
+        ch1_3.setTag(null);
+        list2.add(ch1_3);
+        gru1.setItems(list2);
+
+        list2 = new ArrayList<ExpandListChild>();
+        ExpandListGroup gru2 = new ExpandListGroup();
+        gru2.setName("Edit");
+        ExpandListChild ch2_1 = new ExpandListChild();
+        ch2_1.setName("Undo");
+        ch2_1.setTag(null);
+        list2.add(ch2_1);
+        ExpandListChild ch2_2 = new ExpandListChild();
+        ch2_2.setName("Copy");
+        ch2_2.setTag(null);
+        list2.add(ch2_2);
+        ExpandListChild ch2_3 = new ExpandListChild();
+        ch2_3.setName("Paste");
+        ch2_3.setTag(null);
+        list2.add(ch2_3);
+        gru2.setItems(list2);
+
+        list2 = new ArrayList<ExpandListChild>();
+        ExpandListGroup gru3 = new ExpandListGroup();
+        gru3.setName("Report");
+        ExpandListChild ch3_1 = new ExpandListChild();
+        ch3_1.setName("Daily Report");
+        ch3_1.setTag(null);
+        list2.add(ch3_1);
+        ExpandListChild ch3_2 = new ExpandListChild();
+        ch3_2.setName("Monthly Report");
+        ch3_2.setTag(null);
+        list2.add(ch3_2);
+        ExpandListChild ch3_3 = new ExpandListChild();
+        ch3_3.setName("Annual Report");
+        ch3_3.setTag(null);
+        list2.add(ch3_3);
+        gru3.setItems(list2);
+        list.add(gru1);
+        list.add(gru2);
+        list.add(gru3);
+        return list;
     }
 
     @Override
