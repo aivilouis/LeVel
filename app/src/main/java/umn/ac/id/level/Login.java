eb3ba4b230383b5961315222475965f129a97681
brigittx <brxxx17@gmail.com>
2022-12-15 02:10:12 +0700

    login googlw

--- app/src/main/java/umn/ac/id/level/Login.java
+++ app/src/main/java/umn/ac/id/level/Login.java
@@ -7,11 +7,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
@@ -20,6 +27,8 @@ public class Login extends AppCompatActivity {
     private FirebaseAuth mAuth;
     SharedPreferences sharedPreferences;
     String email, password;
+    private GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,7 @@ public class Login extends AppCompatActivity {
         etEmail = findViewById(R.id.email);
         etPassword = findViewById(R.id.password);
         Button btn = findViewById(R.id.loginBtn);
+        TextView googleSignin = findViewById(R.id.googleSignIn);
         TextView tvSignup = findViewById(R.id.tvSignup);
 
         sharedPreferences = getSharedPreferences("SHARED_PREFS", Context.MODE_PRIVATE);
@@ -43,6 +53,15 @@ public class Login extends AppCompatActivity {
         tvSignup.setOnClickListener(v -> {
             Intent intent = new Intent(Login.this, SignUp.class);
             startActivity(intent);
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        googleSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginGoogle();
+            }
         });
     }
 
@@ -69,6 +88,35 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_LONG).show();
                 }
             });
+        }
+    }
+
+    private void LoginGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            final GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            if (!account.getId().equals("")&&!account.getId().equals(null)){
+                Intent i = new Intent(Login.this, Account.class);
+                startActivity(i);
+                finish();
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_LONG).show();
+            }
+        } catch (ApiException e) {
+            Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_LONG).show();
         }
     }
 
