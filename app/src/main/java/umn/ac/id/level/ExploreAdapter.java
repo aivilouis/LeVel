20e43dc227aea5463cf3dee0bb58c483717bbd59
brigittx <brxxx17@gmail.com>
2022-12-15 18:27:16 +0700

    edit home

--- app/src/main/java/umn/ac/id/level/ExploreAdapter.java
+++ app/src/main/java/umn/ac/id/level/ExploreAdapter.java
@@ -1,8 +1,10 @@
 package umn.ac.id.level;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,7 +39,12 @@ public class ExploreAdapter extends
     protected void onBindViewHolder(@NonNull ExploreAdapter.ExploreViewHolder holder, int position, @NonNull ExploreItem model) {
         holder.user.setText(model.getUser());
         holder.location.setText(model.getLocation());
-        holder.travelDays.setText(model.getTravelDays() + " days");
+        if(model.getTravelDays() == 1){
+            holder.travelDays.setText(model.getTravelDays() + " day");
+        }
+        else{
+            holder.travelDays.setText(model.getTravelDays() + " days");
+        }
         holder.totalCost.setText("Rp " + model.getTotalCost() + ",-");
 
         byte[] decodedString = Base64.decode(model.getLocationImg(), Base64.DEFAULT);
@@ -46,7 +53,6 @@ public class ExploreAdapter extends
 
         new ImageLoadTask("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
                 holder.profileImg).execute();
-
     }
 
     static class ExploreViewHolder extends RecyclerView.ViewHolder {
