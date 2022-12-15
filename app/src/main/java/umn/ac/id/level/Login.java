4deeac22d69d204295a3361376b0dfa4568e5513
brigittx <brxxx17@gmail.com>
2022-12-15 15:20:25 +0700

    update gradle

--- app/src/main/java/umn/ac/id/level/Login.java
+++ app/src/main/java/umn/ac/id/level/Login.java
@@ -1,17 +1,16 @@
 package umn.ac.id.level;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
